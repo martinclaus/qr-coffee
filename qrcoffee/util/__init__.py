ed2d3a89b26a1f6b4f3c850a9915bd67578700fa
Martin Claus <mc.laus@gmx.de>
2014-10-09 21:45:56 +0200

    initial commit of python site-package

---
+++ qrcoffee/util/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Martin Claus'
+__copyright__ = "Copyright 2014"
+__credits__ = ["Martin Claus"]
+__license__ = "GPL"
+__version__ = "2.0"
+__maintainer__ = "Martin Claus"
+__email__ = "mclaus@geomar.de"
+__status__ = "development"
+__version__ = '0.1.0'
